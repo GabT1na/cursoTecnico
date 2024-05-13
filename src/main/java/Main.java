19ae437ca1515e37dff16ee41520e93765c7fdd4
Gabrielly Cristina <167761882+GabT1na@users.noreply.github.com>
2024-05-13 17:13:15 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("----------------------");
+    System.out.println("Lógica Computacional");
+    System.out.println(" ");
+    System.out.println("Professor: Lucas Stasiak");
+    System.out.println("----------------------");
+    System.out.println(" ");
+    System.out.println("----------------------");
+    System.out.println(" ");
+    System.out.println(" No curso de Desenvolvimento de Sistemas temos apenas três matérias tecnicas no primeiro ano, sendo uma delas 'Lógica Computacional' que particularmante é a minha favorita.");
+    System.out.println(" ");
+    System.out.println("No começo foi estranho a parte de Tabela Verdade, mas não demorou muito para entender como funcionava e o quão fácil era fazer. Assim avancei na matéria e estava ansiosa para programar de verdade.");
+    System.out.println(" ");
+    System.out.println("Como antes eu já sabia o básico da programação em tipo C (Arduíno Nano) não tive dificuldade em começar a fazer os trabalhos que foram passados pelo professor.");
+    System.out.println(" ");
+    System.out.println("O que eu mais quero aprender é sobre a formação de jogos e até talvez fazer um, mas por enquanto preciso ter prática com o básico e saber fazer bonito.");
+    System.out.println(" ");
+    System.out.println("----------------------");
+  }
+}
